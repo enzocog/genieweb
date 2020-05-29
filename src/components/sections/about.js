bcaadb4ee6ff58964ba2bbff571c731a0cf5c7ff
enzocog <56004294+enzocog@users.noreply.github.com>
2020-05-29 10:30:48 -0700

    retry build

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -3,7 +3,7 @@ import styled from "styled-components"
 
 import { Section, Container } from "../global"
 import Enzo from "../../images/product/enzo.jpg"
-import Vishesh from "../../images/product/vishesh.jpg"
+import Vishesh from "../../images/product/vishesh.JPG"
 
 const About = () => (
   <Section id="about">
